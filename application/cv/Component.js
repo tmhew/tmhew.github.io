8fee056df8cceb4cb4d82779e26ea39bd641a090
tmhew <5363606+tmhew@users.noreply.github.com>
2023-04-11 19:03:08 -0500

    Create Component.js

---
+++ application/cv/Component.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+  'sap/ui/core/UIComponent'
+], (UIComponent) => {
+  const _UIComponent = UIComponent.extend('com.tmhew.application.cv.Component', {
+    metadata: {
+      interfaces: ['sap.ui.core.IAsyncContentCreation'],
+      manifest: 'json'
+    }
+  })
+  
+  _UIComponent.prototype.init = function () {
+    UIComponent.prototype.init.apply(this, arguments)
+  }
+  
+  return _UIComponent
+})
