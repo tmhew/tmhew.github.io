6de07b1fdc23cc9639409068428d54caa9472e1a
tmhew <5363606+tmhew@users.noreply.github.com>
2023-04-11 19:03:46 -0500

    Update and rename application/cv/Component.js to app/cv/Component.js

--- app/cv/Component.js
+++ app/cv/Component.js
@@ -1,7 +1,7 @@
 sap.ui.define([
   'sap/ui/core/UIComponent'
 ], (UIComponent) => {
-  const _UIComponent = UIComponent.extend('com.tmhew.application.cv.Component', {
+  const _UIComponent = UIComponent.extend('com.tmhew.app.cv.Component', {
     metadata: {
       interfaces: ['sap.ui.core.IAsyncContentCreation'],
       manifest: 'json'
