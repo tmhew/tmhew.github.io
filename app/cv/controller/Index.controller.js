64d53e792f97bd39dc02272912b812ae9c7306c4
tmhew <5363606+tmhew@users.noreply.github.com>
2023-04-21 16:29:50 -0500

    theme changed

--- app/cv/controller/Index.controller.js
+++ app/cv/controller/Index.controller.js
@@ -4,7 +4,14 @@ sap.ui.define([
     const _Controller = Controller.extend('com.tmhew.app.cv.controller.Index')
 
     _Controller.prototype.onThemeButtonPressed = function (event) {
-        console.log(event.getSource())
+        const button = event.getSource()
+        if (button.getIcon() === 'sap-icon://light-mode') {
+            button.setIcon('sap-icon://dark-mode')
+            sap.ui.getCore().applyTheme('sap_horizon_dark')
+        } else {
+            button.setIcon('sap-icon://light-mode')
+            sap.ui.getCore().applyTheme('sap_horizon')
+        }
     }
 
     return _Controller
