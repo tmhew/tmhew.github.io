577f592060ae2268196bb4bdb2db8f8667ba1e38
tmhew <5363606+tmhew@users.noreply.github.com>
2023-04-22 16:43:19 -0500

    mode

--- app/cv/controller/Index.controller.js
+++ app/cv/controller/Index.controller.js
@@ -1,17 +1,30 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], (Controller) => {
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+], (Controller, JSONModel) => {
     const _Controller = Controller.extend('com.tmhew.app.cv.controller.Index')
 
-    _Controller.prototype.onThemeButtonPressed = function (event) {
-        const button = event.getSource()
-        if (button.getIcon() === 'sap-icon://light-mode') {
-            button.setIcon('sap-icon://dark-mode')
+    _Controller.prototype.onInit = function () {
+        const currentTheme = sap.ui.getCore().getConfiguration().getTheme()
+
+        this._viewModel = new JSONModel({
+            'themeMode': currentTheme.endsWith('dark') ? 'dark' : 'light'
+        })
+
+        this.getView().setModel(this._viewModel, 'view')
+    }
+
+    _Controller.prototype.onThemeButtonPressed = function () {
+        let currentThemeMode = this._viewModel.getProperty('/themeMode')
+        currentThemeMode = currentThemeMode === 'light' ? 'dark' : 'light'
+
+        if (currentThemeMode === 'light') {
             sap.ui.getCore().applyTheme('sap_horizon')
         } else {
-            button.setIcon('sap-icon://light-mode')
             sap.ui.getCore().applyTheme('sap_horizon_dark')
         }
+
+        this._viewModel.setProperty('/themeMode', currentThemeMode)
     }
 
     _Controller.prototype.onSourceCodeButtonPressed = function () {
