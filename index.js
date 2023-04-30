f56b187cece71ff4c2ea30dd7919f429b6674c76
tmhew <5363606+tmhew@users.noreply.github.com>
2023-04-30 00:25:26 -0500

    again

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+sap.ui.predefine('com/tmhew/control/Test', [
+    'sap/ui/core/Control',
+    'sap/m/Text'
+], (Control, Text) => {
+    return Control.extend('com.tmhew.control.Test', {
+        renderer: function (rm, control) {
+            rm.openStart('div', control)
+            rm.openEnd()
+            rm.renderControl(new Text({ text: 'hello world' }))
+            rm.close('div')
+        }
+    })
+})
+
 sap.ui.define([
     'sap/ui/core/mvc/XMLView',
     'sap/ui/core/mvc/Controller'
 ], async (XMLView, Controller) => {
-    sap.ui.predefine('com/tmhew/control/Test', [
-        'sap/ui/core/Control',
-        'sap/m/Text'
-    ], (Control, Text) => {
-        return Control.extend('com.tmhew.control.Test', {
-            renderer: function (rm, control) {
-                rm.openStart('div', control)
-                rm.openEnd()
-                rm.renderControl(new Text({ text: 'hello world' }))
-                rm.close('div')
-            }
-        })
-    })
-
     const indexView = await XMLView.create({
         definition: document.getElementById('Index.view.xml').innerHTML
     })
